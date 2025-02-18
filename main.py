d5ed6e38fc67e0c45b069865f65065b5a51a356f
Savi0107 <savithriparameswaran07@gmail.com>
2025-02-18 16:13:25 +0530

    savi's part

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 @app.route('/login')
 def login():
@@ -9,5 +9,14 @@ def login():
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+@app.route('/forgotpass')
+def forgotpass():
+    return render_template('forgotpass.html')
+@app.route('/reset')
+def reset():
+    return render_template('reset.html')
+@app.route('/home')
+def home():
+    return render_template('home.html')
 if __name__ == '__main__':
     app.run(debug=True)
